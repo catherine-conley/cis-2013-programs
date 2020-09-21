46f2e634dd8c0075e1dba0b6606eb1f01a437b03
catherine-conley <70607628+catherine-conley@users.noreply.github.com>
2020-09-20 20:05:37 -0500

    Add files via upload

--- age.js
+++ age.js
@@ -1,12 +1,9 @@
  var floatAge, floatDays, intWeeks, floatMonths, intFortnights
+ /*This program will calculate age in days, months, weeks, and fortnights*/
      floatAge = parseFloat (prompt("Enter Age"));
-     floatDays = parseFloat(floatAge * 365.25);
-     floatMonths= parseFloat(floatAge * 12);
+     floatDays = parseFloat(floatAge * 365.25).toFixed(2);
+     floatMonths= parseFloat(floatAge * 12).toFixed(2);
      intWeeks= parseInt(floatDays/7);
      intFortnights= parseInt(floatDays/14);
-     alert("Age= " +floatAge);
-     alert("Days= " +floatDays);
-     alert("Months= " +floatMonths);
-     alert("Weeks= " +intWeeks);
-     alert("Fortnights= " +intFortnights);
+     alert("Age= " +floatAge)+("Days= " +floatDays)+("Months= " +floatMonths)+("Weeks= " +intWeeks)+("Fortnights= " +intFortnights);
 
