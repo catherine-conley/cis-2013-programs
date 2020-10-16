b79cd1189bf8715da53d2144d8f61f332c83537f
catherine-conley <70607628+catherine-conley@users.noreply.github.com>
2020-10-15 21:21:19 -0500

    Add files via upload

--- dom2013grade.js
+++ dom2013grade.js
@@ -3,52 +3,7 @@ var $ = function (id)
     return document.getElementById(id);
 };
 
-var calculate_click = function (
- {   
-    if (intGradeOption===1)
-{
-    if(floatTotalPts >= 80)
-    {
-        stringFinalGrade = "Pass";
-    }
-    else
-    {
-        stringFinalGrade = "Fail";   
-    }
-}
- }
-else
-{
-    if(floatTotalPts >= 90)
-        {
-            stringFinalGrade = "A";
-        }
-    else
-    {
-        if (floatTotalPts < 90 && floatTotalPts >= 80)
-        {
-            stringFinalGrade = "B";
-        }
-        else
-        {
-            if (floatTotalPts <80 && floatTotalPts >=70)
-            {
-                stringFinalGrade = "C";
-            }
-            else
-            {
-                if (floatTotalPts <70 && floatTotalPts >=60)
-                {
-                    stringFinalGrade = "D";
-                }
-                    else
-                    {
-                        stringFinalGrade = "F";
-                    }
-            }
-        }
-    }
-}) 
+var calculate_click = function ()
 {
     var floatHwPts, floatMidPts, floatFinPts, floatTotalPts, intGradeOption, stringFinalGrade;
 
@@ -57,16 +12,56 @@ else
     floatFinPts = parseFloat($("fin_pts").value);
     floatTotalPts = parseFloat(floatHwPts + floatMidPts + floatFinPts);
     intGradeOption = parseInt($("grade_option").value);
+  
+    if (intGradeOption===1)
+    {
+        if(floatTotalPts >= 80)
+        {
+            stringFinalGrade = "Pass";
+        }
+        else
+        {
+            stringFinalGrade = "Fail";   
+        }
+    }
+    else
+    {
+        if(floatTotalPts >= 90)
+            {
+                stringFinalGrade = "A";
+            }
+        else
+        {
+            if (floatTotalPts < 90 && floatTotalPts >= 80)
+            {
+                stringFinalGrade = "B";
+            }
+            else
+            {
+                if (floatTotalPts <80 && floatTotalPts >=70)
+                {
+                    stringFinalGrade = "C";
+                }
+                else
+                {
+                    if (floatTotalPts <70 && floatTotalPts >=60)
+                    {
+                        stringFinalGrade = "D";
+                    }
+                        else
+                        {
+                            stringFinalGrade = "F";
+                        }
+            }
+        }
+    }
+} 
 
-
-
-
+$("final_grade").value = stringFinalGrade;
+};
 window.onload = function () 
 {
     $("final_grade").value = ""; //blanks the final grade text box upon page load
     $("calculate").onclick = calculate_click; //activates main method when the button is clicked 
     $("hw_pts").focus(); //puts the cursor on the first DOM text input box
 };
-var stringFinalGrade;
-
-$("final_grade").value = stringFinalGrade;
